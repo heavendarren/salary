61de758eebb84979f75becbad5fd948c3a26b79c
wangqingxiang <heavendarren@126.com>
2017-04-18 10:44:17 +0800

    提交模板

--- src/com/yunxiang/salary/ReadExcelUtil.java
+++ src/com/yunxiang/salary/ReadExcelUtil.java
@@ -1,6 +1,8 @@
 package com.yunxiang.salary;
 
 import org.apache.poi.ss.usermodel.*;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -21,7 +23,8 @@ public class ReadExcelUtil {
         SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
         try {
             //同时支持Excel 2003、2007
-            File excelFile = new File("E://test1.xlsx"); //创建文件对象
+            Resource path = new DefaultResourceLoader().getResource("classpath:/");
+            File excelFile = new File("g://test1.xlsx"); //创建文件对象
             FileInputStream is = new FileInputStream(excelFile); //文件流
             Workbook workbook = WorkbookFactory.create(is); //这种方式 Excel 2003/2007/2010 都是可以处理的
             FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
@@ -34,11 +37,8 @@ public class ReadExcelUtil {
                 Sheet sheet = workbook.getSheetAt(s);
                 int rowCount = sheet.getPhysicalNumberOfRows(); //获取总行数
 
-                if(rowCount<3){
-                    continue;
-                }
                 //遍历每一行
-                for (int r = 3; r < rowCount; r++) {
+                for (int r = 0; r < rowCount; r++) {
                     Row row = sheet.getRow(r);
                     if(row==null){
                         continue;
@@ -48,62 +48,66 @@ public class ReadExcelUtil {
                     salary=new ArrayList<>();
                     int cellCount = row.getPhysicalNumberOfCells(); //获取总列数
                     //遍历每一列
+                    System.out.println(cellCount);
 
-                    for (int c = 1; c < cellCount; c++) {
+                    for (int c = 0; c <= 27; c++) {
+
                         Cell cell = row.getCell(c);
-                        if(cell==null){
-                            continue;
-                        }
-                        int cellType = cell.getCellType();
-
                         String cellValue = null;
-                        switch(cellType) {
-                            case Cell.CELL_TYPE_STRING: //文本
-                                cellValue = cell.getStringCellValue();
-                                break;
-                            case Cell.CELL_TYPE_NUMERIC: //数字、日期
-                                if(DateUtil.isCellDateFormatted(cell)) {
-                                    cellValue = fmt.format(cell.getDateCellValue()); //日期型
-                                }
-                                else {
-                                    cellValue = String.valueOf(cell.getNumericCellValue()); //数字
-                                }
-                                break;
-                            case Cell.CELL_TYPE_BOOLEAN: //布尔型
-                                cellValue = String.valueOf(cell.getBooleanCellValue());
-                                break;
-                            case Cell.CELL_TYPE_BLANK: //空白
-                                cellValue = cell.getStringCellValue();
-                                break;
-                            case Cell.CELL_TYPE_ERROR: //错误
-                                cellValue = "错误";
-                                break;
-                            case Cell.CELL_TYPE_FORMULA: //公式
+                        if(cell==null){
+                            cellValue="0.0";
+                        }else{
+                            int cellType = cell.getCellType();
+                            switch(cellType) {
+                                case Cell.CELL_TYPE_STRING: //文本
+                                    cellValue = cell.getStringCellValue();
+                                    break;
+                                case Cell.CELL_TYPE_NUMERIC: //数字、日期
+                                    if(DateUtil.isCellDateFormatted(cell)) {
+                                        cellValue = fmt.format(cell.getDateCellValue()); //日期型
+                                    }
+                                    else {
+                                        cellValue = String.valueOf(cell.getNumericCellValue()); //数字
+                                    }
+                                    break;
+                                case Cell.CELL_TYPE_BOOLEAN: //布尔型
+                                    cellValue = String.valueOf(cell.getBooleanCellValue());
+                                    break;
+                                case Cell.CELL_TYPE_BLANK: //空白
+                                    cellValue = cell.getStringCellValue();
+                                    break;
+                                case Cell.CELL_TYPE_ERROR: //错误
+                                    cellValue = "错误";
+                                    break;
+                                case Cell.CELL_TYPE_FORMULA: //公式
 
-                                try{
-                                    CellValue  value=evaluator.evaluate(cell);
-                                    if(value!=null){
-                                        cellValue=String.valueOf(value.getNumberValue());
-                                    }else{
+                                    try{
+                                        CellValue  value=evaluator.evaluate(cell);
+                                        if(value!=null){
+                                            cellValue=String.valueOf(value.getNumberValue());
+                                        }else{
+                                            cellValue = "错误";
+                                        }
+                                    }catch (Exception e){
+                                        e.printStackTrace();
                                         cellValue = "错误";
                                     }
-                                }catch (Exception e){
-                                    e.printStackTrace();
+                                    break;
+                                default:
                                     cellValue = "错误";
-                                }
-                                break;
-                            default:
-                                cellValue = "错误";
+                            }
                         }
-                        if(c==1){
+
+                        if(c==0){
                             username=cellValue;
                         }else{
                             salary.add(cellValue);
                         }
-//                        System.out.print(cellValue);
+                        System.out.print(cellValue);
+                        System.out.print("    ");
 
                     }
-//                    System.out.println("");
+                    System.out.println("");
                     salaryMap.put(username,salary);
                 }
             }
@@ -122,7 +126,9 @@ public class ReadExcelUtil {
         Map<String,String>  emailMap=new HashMap<String,String>();
         try {
             //同时支持Excel 2003、2007
-            File excelFile = new File("E://test2.xlsx"); //创建文件对象
+
+            Resource path = new DefaultResourceLoader().getResource("classpath:/");
+            File excelFile = new File("g://test2.xlsx"); //创建文件对象
             FileInputStream is = new FileInputStream(excelFile); //文件流
             Workbook workbook = WorkbookFactory.create(is); //这种方式 Excel 2003/2007/2010 都是可以处理的
             FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
@@ -142,6 +148,9 @@ public class ReadExcelUtil {
                         continue;
                     }
                     String username =  row.getCell(0).getStringCellValue();
+                    if(username==null||username.equals("")){
+                        continue;
+                    }
                     String email =  row.getCell(1).getStringCellValue();
                     emailMap.put(username,email);
 //                    System.out.println(username+"<<<<<<<<<<<<>"+email);
